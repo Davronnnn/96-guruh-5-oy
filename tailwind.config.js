7da9c6bd7dbeb6eb9b8f9c6112f0136143985d1b
Davronnn <kifleader@gmail.com>
2023-02-22 21:07:56 +0500

    last

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		colors: {
+			primary: 'blue',
+			blue: '#1fb6ff',
+			purple: '#7e5bef',
+			pink: '#ff49db',
+			orange: '#ff7849',
+			green: '#13ce66',
+			yellow: '#ffc82c',
+			'gray-dark': '#273444',
+			gray: '#8492a6',
+			'gray-light': '#d3dce6',
+		},
+		fontFamily: {
+			sans: ['Graphik', 'sans-serif'],
+			serif: ['Merriweather', 'serif'],
+			test: ['Merriweather', 'serif'],
+		},
+		extend: {},
+	},
+	plugins: [],
+};
